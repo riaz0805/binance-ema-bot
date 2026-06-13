6ad996f45d96d6ada136f4113a18e567cd6295eb
riaz0805 <mdriaj66@gmail.com>
2026-06-13 21:01:26 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,8 @@ import requests
 
 url = "https://api.binance.com/api/v3/exchangeInfo"
 
-data = requests.get(url).json()
+response = requests.get(url, timeout=20)
 
-symbols = []
-
-for s in data["symbols"]:
-    if s["quoteAsset"] == "USDT" and s["status"] == "TRADING":
-        symbols.append(s["symbol"])
-
-print("TOTAL SYMBOLS:", len(symbols))
-print(symbols[:20])
+print("STATUS:", response.status_code)
+print("RAW RESPONSE:")
+print(response.text)
